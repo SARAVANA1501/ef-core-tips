b30b618aebc759dad4d2b009777f3aba80d8acf7
Saravana Kumar <saravanakumar@Apples-MacBook-Pro.local>
2019-08-02 22:59:31 +0530

    Update SQL Query.

--- ProductMasterConsole/SQLScript/sample-data.sql
+++ ProductMasterConsole/SQLScript/sample-data.sql
@@ -1,7 +1,14 @@
+--This query is for PostgresSQL
 DROP TABLE IF EXISTS "product";
 CREATE TABLE "public"."product"
 (
     "Id"          integer     NOT NULL,
     "ProductName" text        NOT NULL,
-    "CreatedDate" timestamptz NOT NULL
+    "CreatedDate" timestamptz NOT NULL,
+    CONSTRAINT "product_Id" PRIMARY KEY ("Id")
 ) WITH (oids = false);
+
+TRUNCATE "product";
+INSERT INTO "product" ("Id", "ProductName", "CreatedDate")
+VALUES (1, 'Mobile', '2019-08-02 16:59:04.772571+00'),
+       (2, 'Laptop', '2019-08-02 16:59:16.698214+00');
